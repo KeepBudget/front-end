521f409d1c1099b69be3505d8f326946eb9ff518
kihyun.kwon <kkh3272@naver.com>
2024-08-14 01:56:05 +0900

    feat: Sentiment & Accident ui 제작

---
+++ src/components/NewsComponent.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="news-component">
+    <div id="news-img">img</div>
+    <div id="news-content">
+      <div id="news-title">
+        뉴스 제목 뉴스 제목 뉴스 제목 뉴스 제목 뉴스 제목 뉴스 제목 뉴스 제목
+        뉴스 제목
+      </div>
+      <div id="news-summary">
+        뉴스 요약~~뉴스 요약~~뉴스 요약~~뉴스 요약~~뉴스 요약~~뉴스 요약~~뉴스
+        요약~~뉴스 요약~~뉴스 요약~~뉴스 요약~~뉴스 요약~~뉴스 요약~~
+      </div>
+      <div>(신문사) (신문시간)</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+#news-component {
+  margin-top: 5px;
+  margin-bottom: 10px;
+  padding: 10px;
+  border: 1px solid #d5d5d5;
+  background-color: white;
+  border-radius: 5px;
+  box-shadow: 1px 1px 2px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+}
+
+#news-img {
+  width: 70px;
+  height: 70px;
+  background-color: #e5e5e5;
+}
+
+#news-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 250px;
+  font-size: small;
+}
+
+#news-title {
+  font-weight: bold;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  margin-bottom: 5px;
+}
+
+#news-summary {
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  margin-bottom: 5px;
+}
+</style>
--- src/views/AccidentView.vue
+++ src/views/AccidentView.vue
@@ -1,13 +1,117 @@
 <template>
-  <div>Accident</div>
+  <div id="accident-view">
+    <CommonHeader />
+    <div id="user">
+      <div id="user-header">
+        <div id="user-header-text"><span>홍길동님</span>의 희망지역</div>
+        <img src="@/assets/png/userEdit.png" alt="editBtn" />
+      </div>
+      <div id="user-body">
+        <div id="user-body-text">
+          <div>서울시 강남구</div>
+        </div>
+      </div>
+    </div>
+    <div id="accident-keyword">키워드 넣기</div>
+    <div id="news-list">
+      <NewsComponent />
+      <NewsComponent />
+      <NewsComponent />
+      <NewsComponent />
+      <NewsComponent />
+      <NewsComponent />
+      <NewsComponent />
+    </div>
+  </div>
 </template>
 
 <script>
+import CommonHeader from '@/components/CommonHeader.vue';
+import NewsComponent from '@/components/NewsComponent.vue';
+
 export default {
+  components: {
+    CommonHeader,
+    NewsComponent,
+  },
   setup() {
     return {};
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+#accident-view {
+  padding: 20px;
+  width: 100vw;
+  height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  background-image: linear-gradient(to bottom, #f5f5f5 15%, white 15%);
+}
+
+#user {
+  margin-top: 10px;
+  margin-bottom: 10px;
+  width: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+  background-color: #d5d5d5;
+  border-radius: 10px;
+}
+
+#user-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+#user-header-text span {
+  font-size: x-large;
+  font-weight: bold;
+  color: #60584c;
+}
+
+#user-body {
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+}
+
+#user-body img {
+  width: 20vw;
+}
+
+#user-body-text {
+  font-size: medium;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+}
+
+#user-body-text span {
+  font-weight: bold;
+  margin-right: 15px;
+}
+
+#accident-keyword {
+  width: 100%;
+  height: 200px;
+  background-color: #e5e5e5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#news-list {
+  height: 420px;
+  overflow: scroll;
+}
+
+#news-list::-webkit-scrollbar {
+  display: none;
+  /* for Chrome, Safari, and Opera */
+}
+</style>
